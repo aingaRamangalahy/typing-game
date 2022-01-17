0d37d89a39ef0cbbb59ba8c3c0f8924483d2d6bf
ainga Ramangalahy <aingaramangalahy@gmail.com>
2022-01-17 12:54:33 +0300

    first step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'typing-game';
+  randomText =
+    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab repudiandae vero, architecto possimus';
+  enteredText = '';
+  emitError: boolean = false
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+
+  compare(randomLetter, enteredLetter) {
+    if (!enteredLetter) return 'pending';
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
